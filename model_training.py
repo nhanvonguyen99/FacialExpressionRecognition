8d96ba4c0b677c2b6ef0858095334150cad56d48
Nguyen Vo Nhan <nhan.nguyenvo1@gmail.com>
2020-06-20 11:57:57 +0700

    Visualize regularization parameter graph

--- model_training.py
+++ model_training.py
@@ -18,6 +18,54 @@ def main():
         joblib.dump(images, "data_save/images.sav")
         joblib.dump(labels, "data_save/labels.sav")
     X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=2)
+    # Cs = []
+    # accuracies = []
+    #
+    # accuracies1 = []
+    #
+    # accuracies2 = []
+    #
+    # accuracies3 = []
+    #
+    # i = 0.1
+    # while i < 3.1:
+    #     model = SVC(kernel="rbf", C=i)
+    #     model.fit(X_train, y_train)
+    #     pred = model.predict(X_test)
+    #     accuracies.append(accuracy_score(y_test, pred))
+    #
+    #     model1 = SVC(kernel="linear", C=i)
+    #     model1.fit(X_train, y_train)
+    #     pred = model1.predict(X_test)
+    #     accuracies1.append(accuracy_score(y_test, pred))
+    #
+    #     model2 = SVC(kernel="poly", C=i)
+    #     model2.fit(X_train, y_train)
+    #     pred = model2.predict(X_test)
+    #     accuracies2.append(accuracy_score(y_test, pred))
+    #
+    #     model3 = SVC(kernel="sigmoid", C=i)
+    #     model3.fit(X_train, y_train)
+    #     pred = model3.predict(X_test)
+    #     accuracies3.append(accuracy_score(y_test, pred))
+    #
+    #     Cs.append(i)
+    #     i += 0.1
+    #
+    # import matplotlib.pyplot as plt
+    # plt.plot(Cs, accuracies, "r", label="rbf")
+    # plt.plot(Cs, accuracies1, "b", label="linear")
+    # plt.plot(Cs, accuracies2, "g", label="polynomial")
+    # plt.plot(Cs, accuracies3, "y", label="sigmoid")
+    #
+    # plt.xlabel("Regularization parameter (C)")
+    # plt.ylabel("Accuracy")
+    # plt.title("Regularization parameter regarding to accuracy")
+    #
+    # plt.legend()
+    #
+    # plt.show()
+
     model = SVC(kernel="rbf")
     model.fit(X_train, y_train)
     pred = model.predict(X_test)
